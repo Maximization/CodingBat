71f0a0549667af604e0ffde77ba3256636e0590d
Maxim Orlov <Maxim Orlov@145.118.115.230>
2014-11-18 03:20:28 +0100

    Added first problems

---
+++ src/warmup1/MonkeyTrouble.java
@@ -0,0 +1,10 @@
+package warmup1;
+
+/**
+ * created by Maxim Orlov on 18 Nov 2014
+ */
+public class MonkeyTrouble {
+	public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+		return (aSmile == bSmile);
+	}
+}
---
+++ src/warmup1/SleepIn.java
@@ -0,0 +1,10 @@
+package warmup1;
+
+/**
+ * created by Maxim Orlov on 18 Nov 2014
+ */
+public class SleepIn {
+	public boolean sleepIn(boolean weekday, boolean vacation) {
+		return (!weekday || vacation);
+	}
+}
